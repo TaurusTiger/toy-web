0b0b16fc97791df45ad85168aa4f40e79acd9477
tigershi0507 <tigershi0507@gmail.com>
2022-12-27 17:32:47 +0800

    增加了interface的断言的一个例子

--- examples/my_lesson/interface_example.go
+++ examples/my_lesson/interface_example.go
@@ -11,6 +11,7 @@ import (
 /*
 这里实现了io.Writer这个接口（实现了Write这个方法），所以UpperWriter这个结构类型可以被Fprintln当做第一个参数使用
 */
+// 又重新的封装了一下io.Writer，自定义的Write收取参数，然后再调用默认的io.Writer实现的对象的Wirte方法
 type UpperWriter struct {
 	io.Writer
 }
@@ -29,8 +30,48 @@ func (s UpperString) String() string {
 	return strings.ToUpper(string(s))
 }
 
+/*
+interface的断言
+*/
+type I interface {
+	Get() int
+	Set(int)
+}
+
+type S struct {
+	Age int
+}
+
+func (s S) Get() int {
+	return s.Age
+}
+
+func (s *S) Set(age int) {
+	s.Age = age
+}
+
+func f(i I) {
+	i.Set(10)
+	fmt.Println(i.Get())
+}
+
 func main() {
 	fmt.Fprintln(&UpperWriter{os.Stdout}, "hello, world")
 
 	fmt.Fprintln(os.Stdout, UpperString("hello, tiger"))
+
+	s := S{}
+	var i I //声明 i
+	i = &s  //赋值 s 到 i
+	f(i)
+
+	if t, ok := i.(*S); ok {
+		fmt.Println("s.implement I ", t, ok)
+	}
+
+	switch i.(type) {
+	case *S:
+		fmt.Println("ok")
+	}
+
 }
