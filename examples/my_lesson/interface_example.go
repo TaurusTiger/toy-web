a40d00ddd148d52661aebba79505bf7bbefc7f48
tigershi0507 <tigershi0507@gmail.com>
2022-12-27 14:27:04 +0800

    补充了说明

--- examples/my_lesson/interface_example.go
+++ examples/my_lesson/interface_example.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*
-这里使用了
+这里实现了io.Writer这个接口（实现了Write这个方法），所以UpperWriter这个结构类型可以被Fprintln当做第一个参数使用
 */
 type UpperWriter struct {
 	io.Writer
@@ -19,6 +19,10 @@ func (w *UpperWriter) Write(data []byte) (n int, err error) {
 	return w.Writer.Write(bytes.ToUpper(data))
 }
 
+/*
+任意隐式满足 fmt.Stringer 接口的对象都可以打印，不满足 fmt.Stringer 接口的依然可以通过反射的技术打印
+对于每个要打印的对象，如果满足了 fmt.Stringer 接口，则默认使用对象的 String 方法返回的结果打印
+*/
 type UpperString string
 
 func (s UpperString) String() string {
